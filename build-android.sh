4c62de0884b0adaa1868808ab6d92aedc2e5f621
jonmcclung <jonmcclung@gmail.com>
2018-07-26 09:42:00 -0500

    added x86 and updated filesystem to latest branch

--- build-android.sh
+++ build-android.sh
@@ -134,7 +134,7 @@ echo "Building boost version: $BOOST_VER1.$BOOST_VER2.$BOOST_VER3"
 # -----------------------
 
 BOOST_DOWNLOAD_LINK="http://downloads.sourceforge.net/project/boost/boost/$BOOST_VER1.$BOOST_VER2.$BOOST_VER3/boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fboost%2Ffiles%2Fboost%2F${BOOST_VER1}.${BOOST_VER2}.${BOOST_VER3}%2F&ts=1291326673&use_mirror=garr"
-BOOST_TAR="boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}.tar.bz2"
+BOOST_TAR="boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}.tar.gz"
 BOOST_DIR="boost_${BOOST_VER1}_${BOOST_VER2}_${BOOST_VER3}"
 BUILD_DIR="./build/"
 
@@ -360,9 +360,9 @@ if [ ! -d $PROGDIR/$BOOST_DIR ]
 then
 	echo "Unpacking boost"
 	if [ "$OPTION_PROGRESS" = "yes" ] ; then
-		pv $PROGDIR/$BOOST_TAR | tar xjf - -C $PROGDIR
+		pv $PROGDIR/$BOOST_TAR | tar xzf - -C $PROGDIR
 	else
-		tar xjf $PROGDIR/$BOOST_TAR
+		tar xzf $PROGDIR/$BOOST_TAR
 	fi
 fi
 
--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 export CXXFLAGS="-std=c++11"
 
-./build-android.sh $ANDROID_NDK_ROOT --with-libraries=thread,filesystem,date_time,system,atomic | grep -v -F "common.copy"
+./build-android.sh $ANDROID_NDK_ROOT --arch=armeabi-v7a,x86 --with-libraries=thread,filesystem,date_time,system,atomic | grep -v -F "common.copy"
 
