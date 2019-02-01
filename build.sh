2b2b4e68f693b1db313ba549170d4d6ea72da6ec
Jon McClung <jonmcclung@gmail.com>
2019-02-01 12:30:46 -0600

    adds 64-bit

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export CXXFLAGS="-std=c++14"
 
 ./build-android.sh \
         "${ANDROID_NDK_ROOT}" \
-        --arch=armeabi-v7a,x86 \
+        --arch=armeabi-v7a,x86,arm64-v8a,x86_64 \
         --with-libraries=thread,filesystem,date_time,system,atomic \
         --boost=1.68.0 \
     | grep -v -F "common.copy"
