4e7ed0bd4e0f4f70922e3294a56a2417839665d0
Jon McClung <jonmcclung@gmail.com>
2019-07-31 14:42:30 +0000

    adds regex

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ export CXXFLAGS="-std=c++14"
 ./build-android.sh \
         "${ANDROID_NDK_ROOT}" \
         --arch=armeabi-v7a,x86,arm64-v8a,x86_64 \
-        --with-libraries=thread,filesystem,date_time,system,atomic \
+        --with-libraries=thread,filesystem,date_time,system,atomic,regex \
         --boost=1.68.0 \
     | grep -v -F "common.copy"
 
